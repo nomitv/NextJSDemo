7095b7166fdff6e7a9b73ddce3a21b0dd6f0c71f
Nomit Verma <nomitverma1@gmail.com>
2020-11-27 14:39:33 +0530

    Fix imports for the fuse template to run with NextJS

---
+++ pages/src/components/app/auth/store/actions/index.js
@@ -0,0 +1,3 @@
+export {LOGIN_ERROR, LOGIN_SUCCESS, submitLogin, submitLoginWithFireBase} from './login.actions';
+export {REGISTER_ERROR, REGISTER_SUCCESS,} from './register.actions';
+export {SET_USER_DATA, REMOVE_USER_DATA, USER_LOGGED_OUT, setUserDataAuth0, setUserDataFirebase, createUserSettingsFirebase, setUserData, updateUserSettings, updateUserShortcuts, removeUserData, logoutUser} from './user.actions';
---
+++ pages/src/components/app/fuse-layouts/shared-components/quickPanel/store/reducers/quickPanel.reducer.js
@@ -0,0 +1,33 @@
+import * as Actions from '../actions';
+import {GET_QUICK_PANEL_DATA, TOGGLE_QUICK_PANEL} from '../actions/quickPanel.actions'
+
+const initialState = {
+    state: false,
+    data : null
+};
+
+const quickPanel = function (state = initialState, action) {
+    switch ( action.type )
+    {
+        case Actions.GET_QUICK_PANEL_DATA:
+        {
+            return {
+                ...state,
+                data: action.payload
+            };
+        }
+        case Actions.TOGGLE_QUICK_PANEL:
+        {
+            return {
+                ...state,
+                state: !state.state
+            };
+        }
+        default:
+        {
+            return state;
+        }
+    }
+};
+
+export default quickPanel;
